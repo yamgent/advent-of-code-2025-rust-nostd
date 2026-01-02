82775deba1824a878cd6a9bf3596b3f1079a6cca
Maurice Wangleng Tan <tanwangleng@outlook.com>
2026-01-02 20:51:21 +0800

    Day 1: transform to nostd capable

--- src/bin/day01/main.rs
+++ src/bin/day01/main.rs
@@ -1,4 +1,175 @@
+#![no_std]
+#![feature(maybe_uninit_array_assume_init)]
+
+use core::{fmt, mem::MaybeUninit, str::FromStr};
+
 const ACTUAL_INPUT: &str = include_str!("../../../actual_inputs/2025/01/input.txt");
+
+struct ArrayCapMaxed;
+
+struct ArrayVec<T, const N: usize> {
+    buffer: [T; N],
+    used: usize,
+}
+
+impl<T, const N: usize> AsRef<[T]> for ArrayVec<T, N> {
+    fn as_ref(&self) -> &[T] {
+        &self.buffer[..self.used]
+    }
+}
+
+impl<T: fmt::Debug, const N: usize> fmt::Debug for ArrayVec<T, N> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("ArrayVec")
+            .field("buffer", &&self.buffer[..self.used])
+            .field("used", &self.used)
+            .finish()
+    }
+}
+
+impl<T: PartialEq, const N: usize> PartialEq for ArrayVec<T, N> {
+    fn eq(&self, other: &Self) -> bool {
+        self.used == other.used && self.buffer[..self.used] == other.buffer[..other.used]
+    }
+}
+
+impl<T, const N: usize> ArrayVec<T, N> {
+    fn new() -> Self {
+        let buffer = [const { MaybeUninit::uninit() }; N];
+        // SAFETY: ArrayVec API will ensure that uninitialized elements are never accessed.
+        let buffer = unsafe { MaybeUninit::array_assume_init(buffer) };
+
+        Self { buffer, used: 0 }
+    }
+
+    fn push(&mut self, value: T) -> Result<(), ArrayCapMaxed> {
+        if self.used >= self.buffer.len() {
+            Err(ArrayCapMaxed)
+        } else {
+            self.buffer[self.used] = value;
+            self.used += 1;
+            Ok(())
+        }
+    }
+}
+
+// TODO: Unlike Rust's RawVec, we haven't found a way to do T::IS_ZST, so we cannot
+// use ?Sized yet. Work on removing this constraint later
+impl<T, const N: usize> FromIterator<T> for ArrayVec<T, N> {
+    fn from_iter<U: IntoIterator<Item = T>>(iter: U) -> Self {
+        let (buffer, used) = iter.into_iter().fold(
+            ([const { MaybeUninit::uninit() }; N], 0),
+            |(mut buffer, mut used), item| {
+                if used >= buffer.len() {
+                    // TODO: Need to handle this properly, cannot panic on OOM
+                    todo!()
+                }
+
+                buffer[used].write(item);
+                used += 1;
+                (buffer, used)
+            },
+        );
+
+        Self {
+            // SAFETY: 0..used are initialized. used..buf.len() are not, but
+            // the ArrayVec API will ensure that they are initialized if the
+            // vec grows and they start getting used.
+            buffer: unsafe { MaybeUninit::array_assume_init(buffer) },
+            used,
+        }
+    }
+}
+
+struct ArrayVecIntoIter<T, const N: usize> {
+    buffer: [Option<T>; N],
+    used: usize,
+    current: usize,
+}
+
+impl<T, const N: usize> Iterator for ArrayVecIntoIter<T, N> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current >= self.used {
+            None
+        } else {
+            let next = self.buffer[self.current]
+                .take()
+                .expect("each element is only visited once");
+            self.current += 1;
+            Some(next)
+        }
+    }
+}
+
+impl<T, const N: usize> IntoIterator for ArrayVec<T, N> {
+    type Item = T;
+
+    type IntoIter = ArrayVecIntoIter<T, N>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let buffer = self
+            .buffer
+            .into_iter()
+            .enumerate()
+            .filter(|(idx, _)| *idx < self.used)
+            .fold(
+                [const { MaybeUninit::uninit() }; N],
+                |mut buffer, (idx, item)| {
+                    buffer[idx].write(Some(item));
+                    buffer
+                },
+            );
+
+        Self::IntoIter {
+            // SAFETY: 0..used is initialized, and ArrayVecIntoIter will never access
+            // elements outside of 0..used
+            buffer: unsafe { MaybeUninit::array_assume_init(buffer) },
+            used: self.used,
+            current: 0,
+        }
+    }
+}
+
+struct ArrayString<const N: usize> {
+    buffer: ArrayVec<u8, N>,
+}
+
+impl<const N: usize> FromIterator<char> for ArrayString<N> {
+    fn from_iter<T: IntoIterator<Item = char>>(iter: T) -> Self {
+        let (buffer, used) = iter
+            .into_iter()
+            .fold(([0; N], 0), |(mut buffer, mut used), ch| {
+                let mut ch_buffer = [0; 4];
+                ch.encode_utf8(&mut ch_buffer)
+                    .as_bytes()
+                    .iter()
+                    .for_each(|byte| {
+                        if used >= buffer.len() {
+                            // TODO: Need to handle this properly, cannot panic on OOM
+                            todo!()
+                        }
+                        buffer[used] = *byte;
+                        used += 1;
+                    });
+
+                (buffer, used)
+            });
+
+        Self {
+            buffer: ArrayVec { buffer, used },
+        }
+    }
+}
+
+impl<const N: usize> ArrayString<N> {
+    fn parse<F: FromStr>(&self) -> Result<F, F::Err> {
+        // SAFETY: ArrayString is maintained to be utf8 compatible
+        let str_ref = unsafe { str::from_utf8_unchecked(AsRef::<[u8]>::as_ref(&self.buffer)) };
+        str_ref.parse()
+    }
+}
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 enum Instruction {
@@ -12,7 +183,7 @@ impl Instruction {
         let amount = line
             .chars()
             .skip(1)
-            .collect::<String>()
+            .collect::<ArrayString<20>>()
             .parse()
             .expect("a number");
 
@@ -26,15 +197,15 @@ impl Instruction {
     }
 }
 
-fn parse_input(input: &str) -> Vec<Instruction> {
+fn parse_input<const N: usize>(input: &str) -> ArrayVec<Instruction, N> {
     input.trim().lines().map(Instruction::parse).collect()
 }
 
 const START_NUMBER: i32 = 50;
 const TOTAL_NUMBERS: i32 = 100;
 
-fn p1(input: &str) -> String {
-    parse_input(input)
+fn p1(input: &str) -> u32 {
+    parse_input::<5000>(input)
         .into_iter()
         .fold((START_NUMBER, 0u32), |mut acc, line| {
             let amount = match line {
@@ -51,11 +222,10 @@ fn p1(input: &str) -> String {
             acc
         })
         .1
-        .to_string()
 }
 
-fn p2(input: &str) -> String {
-    parse_input(input)
+fn p2(input: &str) -> u32 {
+    parse_input::<5000>(input)
         .into_iter()
         .fold((START_NUMBER, 0u32), |mut acc, line| {
             match line {
@@ -80,12 +250,12 @@ fn p2(input: &str) -> String {
             acc
         })
         .1
-        .to_string()
 }
 
 fn main() {
-    println!("{}", p1(ACTUAL_INPUT));
-    println!("{}", p2(ACTUAL_INPUT));
+    // TODO: Restore this once we have ability to output to console
+    // println!("{}", p1(ACTUAL_INPUT));
+    // println!("{}", p2(ACTUAL_INPUT));
 }
 
 #[cfg(test)]
@@ -94,14 +264,20 @@ mod tests {
 
     #[test]
     fn test_instructions() {
+        let mut expected = ArrayVec::<Instruction, 2>::new();
+        expected.push(Instruction::Left(20));
+        expected.push(Instruction::Right(30));
+
         assert_eq!(
-            parse_input(
+            parse_input::<2>(
                 r"
 L20
 R30
 "
             ),
-            vec![Instruction::Left(20), Instruction::Right(30)]
+            // TODO: Kinda sad that we cannot use vec![], we should implement this one day
+            // vec![Instruction::Left(20), Instruction::Right(30)]
+            expected
         );
     }
 
@@ -120,17 +296,17 @@ L82
 
     #[test]
     fn test_p1_sample() {
-        assert_eq!(p1(SAMPLE_INPUT), "3");
+        assert_eq!(p1(SAMPLE_INPUT), 3);
     }
 
     #[test]
     fn test_p1_actual() {
-        assert_eq!(p1(ACTUAL_INPUT), "1066");
+        assert_eq!(p1(ACTUAL_INPUT), 1066);
     }
 
     #[test]
     fn test_p2_sample() {
-        assert_eq!(p2(SAMPLE_INPUT), "6");
+        assert_eq!(p2(SAMPLE_INPUT), 6);
     }
 
     #[test]
@@ -154,7 +330,7 @@ L82
         .for_each(|(input, expected)| {
             assert_eq!(
                 p2(input),
-                expected.to_string(),
+                expected,
                 "{}, {}",
                 input.replace("\n", ";"),
                 expected
@@ -164,6 +340,6 @@ L82
 
     #[test]
     fn test_p2_actual() {
-        assert_eq!(p2(ACTUAL_INPUT), "6223");
+        assert_eq!(p2(ACTUAL_INPUT), 6223);
     }
 }
